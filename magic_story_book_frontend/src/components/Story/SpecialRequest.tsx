e9bbc22ccd8986c5f80b7f9dac6eddf697aa60ef
Vida Maleki <vida.basmani@gmail.com>
2024-08-05 09:35:02 -0700

    added age as a drop down

--- magic_story_book_frontend/src/components/Story/SpecialRequest.tsx
+++ magic_story_book_frontend/src/components/Story/SpecialRequest.tsx
@@ -29,24 +29,42 @@ const SpecialRequest: React.FC<SpecialRequestProps> = ({
     <div className="special-request-card">
       <div className="title-row">
         <h2>Any Special Request?</h2>
-        <div className="dropdown-container">
-          <label htmlFor="word-count">Word Count:</label>
-          <select
-            id="word-count"
-            name="wordCount"
-            value={inputs.wordCount}
-            onChange={handleInputChange}
-          >
-            <option value="100">100 words</option>
-            <option value="200">200 words</option>
-            <option value="300">300 words</option>
-            <option value="400">400 words</option>
-            <option value="500">500 words</option>
-          </select>
+        <div className="dropdowns-wrapper">
+          <div className="dropdown-container">
+            <label htmlFor="word-count">Word Count:</label>
+            <select
+              id="word-count"
+              name="wordCount"
+              value={inputs.wordCount}
+              onChange={handleInputChange}
+            >
+              <option value="100">100 words</option>
+              <option value="200">200 words</option>
+              <option value="300">300 words</option>
+              <option value="400">400 words</option>
+              <option value="500">500 words</option>
+            </select>
+          </div>
+          <div className="dropdown-container">
+            <label htmlFor="age-selection">Child's Age range:</label>
+            <select
+              id="age-selection"
+              name="childAge"
+              value={inputs.age}
+              onChange={handleInputChange}
+            >
+              <option value="" disabled>
+                Select age
+              </option>
+              <option value="3-5">3-5 years</option>
+              <option value="5-7">5-7 years</option>
+              <option value="7-9">7-9 years</option>
+            </select>
+          </div>
         </div>
       </div>
       <p>
-        Your description will be used to guide the story which ever way you
+        Your description will be used to guide the story whichever way you
         please.
       </p>
       <div className="special-request-content">
